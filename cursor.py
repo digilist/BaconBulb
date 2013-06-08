c319d06174ce80cbca502c9ae6a14318fae7dc24
Blutadler <rageqq89@gmail.com>
2013-06-08 23:54:04 +0200

    leckerer bacon cursor :D

---
+++ cursor.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import sfml as sf
+
+# load cursor icon
+texture = sf.Texture.from_file("gfx/cursor.png")
+cursorIcon = sf.Sprite(texture)
+
+class Cursor(sf.Drawable):
+	def __init__(self, window):
+		self._window = window
+		self._icon = cursorIcon
+
+	def setPosition(self,pos):
+		self._icon.position = pos -(14,16)
+
+	def draw(self, target, states):
+		target.draw(self._icon, states)
