e72913e51609cff152cd92dadb34b67099984b84
Markus Fasselt <markus.fasselt@gmail.com>
2013-06-08 18:31:01 +0200

    test

---
+++ animations.py
@@ -0,0 +1,38 @@
+import sfml as sf
+import settings
+
+class Animation(sf.Drawable):
+    def __init__(self, loop = 0):
+        sf.Drawable.__init__(self)
+
+        self._loop = loop
+        self._frames = []
+
+        self._current_frame = 0
+        self._timer = 0
+
+    def addFrame(self, timer, canvas):
+        self._frames.append({
+            "timer": timer,
+            "canvas": canvas
+        })
+
+    def draw(self, target, states):
+        current_frame = self._frames[self._current_frame];
+
+        test = sf.Text("BaconBulb", settings.font, 50)
+        target.draw(current_frame["canvas"], stages)
+        print target
+        #target.draw(test, stages)
+        print current_frame["canvas"]
+
+        self._timer += 1
+        if(self._timer >= current_frame.timer):
+            self.timer = 0
+            self._current_frame += 1
+
+        if(self._current_frame >= len(self._frames)):
+            if(self._loop):
+                self._current_frame = 0
+            else:
+                self._current_frame = len(self._frames) - 1
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 import sfml as sf
 from game_menu import GameMenu
-from meter import Meter
+from animations import Animation
 import settings
 import time
 
@@ -14,13 +14,21 @@ class Startup():
         self._window = sf.RenderWindow(sf.VideoMode(settings.windowWidth, settings.windowHeight), "BaconBulb")
         self._game_menu = GameMenu(self._window)
 
+        test = sf.Text("BaconBulb", settings.font, 50)
+        test2 = sf.Text("Blub", settings.font, 50)
+
+        self._animation = Animation()
+        self._animation.addFrame(1000, test)
+        self._animation.addFrame(1000, test2)
+
     def run(self):
         while self._window.is_open:
             self.loop()
 
     def loop(self):
         self._window.clear(sf.Color.BLACK)
-        self._game_menu.dispatch()
+        self._window.draw(self._animation)
+        #self._game_menu.dispatch()
         self._game_menu.handle_events()
         self._window.display()
 
