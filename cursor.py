3efb13f7d1c983ada20ad3215ae26481a60477a7
Blutadler <rageqq89@gmail.com>
2013-06-09 01:32:13 +0200

    gut rotierender bacon

--- cursor.py
+++ cursor.py
@@ -2,17 +2,20 @@
 
 import sfml as sf
 
-# load cursor icon
-texture = sf.Texture.from_file("assets/cursor.png")
-cursorIcon = sf.Sprite(texture)
-
 class Cursor(sf.Drawable):
-	def __init__(self, window):
-		self._window = window
-		self._icon = cursorIcon
+    def __init__(self, window):
+        # load cursor icon
+        texture = sf.Texture.from_file("assets/cursor.png")
+        
+        self._window = window
+        self._icon = sf.Sprite(texture)
 
-	def setPosition(self,pos):
-		self._icon.position = pos -(14,16)
+    def setPosition(self,pos):
+        self._icon.position = pos
 
-	def draw(self, target, states):
-		target.draw(self._icon, states)
+    def rotate(self, angle):
+        self._icon.origin = (12,16)
+        self._icon.rotate(angle)
+
+    def draw(self, target, states):
+        target.draw(self._icon, states)
